c25de551e4a31e007a68b0eef16ff23d0d635d52
Axeld <spb-webmaster@mail.ru>
2026-01-13 13:23:13 +0300

    13-yan-2026-3

--- resources/views/components/current-information/current-information-component.blade.php
+++ resources/views/components/current-information/current-information-component.blade.php
@@ -5,8 +5,7 @@
     <div class="current_information__left">
         <h1 class="h1 up">Наши <span>экскурсии</span></h1>
         <div class="ci__title">Выберите идеальный маршрут для вашего путешествия.</div>
-        <div class="ci__subtitle">Судоходная компания «Аква-экскурс» проводит регулярные дневные
-            теплоходные экскурсии.</div>
+        <div class="ci__subtitle">Судоходная компания «Аква-экскурс» проводит регулярные дневные теплоходные экскурсии и ночные прогулки на разводку мостов.</div>
     </div>
     <div class="current_information__right">
 
